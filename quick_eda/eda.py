d61d1e8f668b49ba26ed1efcc919a8770ddc654d
pengyu.wang <pengyu.wang@bluefocus.com>
2020-05-27 16:04:59 +0800

    1.transfer to Python3 ; 2.improved readability; 3.parameters modified

--- quick_eda/eda.py
+++ quick_eda/eda.py
@@ -2,8 +2,8 @@ from IPython.core.pylabtools import figsize
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def quick_eda(df,describe=False,force_cat=None,re = False):
-    print df.info()
+def quick_eda(df,describe=False,force_cat=False,return_value = False):
+    print(df.info())
     misslist = []
     numlist = []
     objlist = []
@@ -13,30 +13,37 @@ def quick_eda(df,describe=False,force_cat=None,re = False):
     for c in df.columns:
         if c in objlist: continue
         if c.endswith('id') or c.endswith('date'):continue
-        if float(len(df[df[c].isnull()]))/len(df)>0.5: print'\n%s contains too many missing values.Drop.'%c; misslist.append(c);continue
+        miss_rate = (len(df[df[c].isnull()])*1.0)/len(df)
+        if miss_rate>0.5: 
+            print('\n{0} contains too many missing values.{1:.2%} is missing.'.format(c,miss_rate)); 
+            misslist.append(c);
+            continue
         
         dfcopy[c]=dfcopy[c].fillna(0)
         try: dfcopy[c] = dfcopy[c].astype(int);numlist.append(c)
         except: objlist.append(c)
     
-    if describe!=False: print df[numlist].describe()
+    if describe!=False: print(df[numlist].describe())
     ln =len(numlist)
     n=0
     height = ln/4+1
     figsize(4*8,height*8)
     plt.rc('font', size=12)
     plt.style.use("fivethirtyeight")
-    print "\nNumberical Variables:",numlist
+    print("\nNumberical Variables:",numlist)
     for c in numlist:
         n+=1
         p1 = plt.subplot(height,4,n)
         p1.hist(dfcopy[c])
         p1.set_title(c)
 
-    print "\nCategory Variables:",objlist
+    print("\nCategory Variables:",objlist)
     for o in objlist:
-        print "\nCategorical Variable: %s"%o
-        if df[o].nunique()<50: print df[o].value_counts()
-        else: print "\n%s contains too many unique values. Can't print."%o 
+        print("\nCategorical Variable: %s"%o)
+        if df[o].nunique()<50: 
+            print(df[o].value_counts())
+        else: 
+            print("\n%s contains %s unique values. Too many, will not be printed."%(df[o].nunique(),o))
     plt.show()
-    if re==True:  return misslist,numlist,objlist
+    if return_value ==True:  
+        return misslist,numlist,objlist
